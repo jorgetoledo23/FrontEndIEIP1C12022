df871aa85cbc565d44abe180217066d3308d6e92
Jorge <jorge.toledo.6@hotmail.com>
2022-05-24 16:28:05 -0400

    24 Mayo 2

--- Unidad 2/ejercicio1.js
+++ Unidad 2/ejercicio1.js
@@ -1,3 +1,5 @@
+//alert("asdasd")
+
 const Joe = {
     "Nombre" : "Joe",
     "Apellidos" : "Amstrong Luxer",
@@ -21,7 +23,34 @@ const Neil = {
 }
 const Personas = [Joe,Ann,Neil];
 
+
 /* Determinar cantidad Personas Mayores de edad */
+let mayores = 0;
+Personas.forEach(p => {
+    if(p.Age >= 18){
+        mayores += 1
+    }
+});
+console.log("Mayores: " + mayores)
+//console.log("Mayores: ${mayores}")
+
 /* Determinar quienes tienen como preferencia musical el Rock */
+
+const root = document.getElementById("root");
+const ul = document.createElement("ul");
+root.className = "container";
+ul.className = "list"
+
+Personas.forEach(p => {
+    p.PreferenciasMusicales.forEach(m => {
+        if(m == "Rock"){
+            //console.log(p.Nombre + " Prefiere el Rock")
+            const li = document.createElement("li");
+            li.innerHTML = p.Nombre + " Prefiere el Rock"
+            ul.appendChild(li)
+        }
+    })
+})
+root.appendChild(ul);
 
 /* Mostrar Resultado en una Pagina Web */
