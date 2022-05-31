8846cfded60ab6e4f2fde0af3414348352d3d01c
Jorge <jorge.toledo.6@hotmail.com>
2022-05-31 14:44:41 -0400

    Clase 31

--- Unidad 2/DOM Interaccion/data.js
+++ Unidad 2/DOM Interaccion/data.js
@@ -3,7 +3,8 @@ const Joe = {
     "Apellidos" : "Amstrong Luxer",
     "Age" : 25,
     "Ciudad" : "Curico",
-    "PreferenciasMusicales" : ["Pop", "Reggae", "Rock"]
+    "PreferenciasMusicales" : ["Pop", "Reggae", "Rock"],
+    "ProfileImg" : "Url"
 }
 const Ann = {
     "Nombre" : "Ann",
@@ -21,3 +22,32 @@ const Neil = {
 }
 
 const Personas = [Joe,Ann,Neil];
+
+
+const root = document.getElementById("root");
+
+for (let i = 0; i < Personas.length; i++) {
+    
+    const Item = Personas[i];
+
+    const card = document.createElement("div");
+    card.classList.add("card")
+    card.classList.add("col-md-3")
+    card.classList.add("p-3")
+    card.style.height = "300px"
+
+    const tituloName = document.createElement("h2")
+    tituloName.innerHTML = Item.Nombre;
+    tituloName.classList.add("text-center")
+    card.appendChild(tituloName);
+
+    const tituloApe = document.createElement("p")
+    tituloApe.className = "fst-italic"
+    tituloApe.classList.add("text-center")
+    tituloApe.innerHTML = Item.Apellidos
+
+    card.appendChild(tituloApe)
+
+    root.appendChild(card)   
+}
+
