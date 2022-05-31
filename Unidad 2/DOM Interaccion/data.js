d6c00d329cc1e8cb25a85a7b36dd4b87c15e580a
Jorge <jorge.toledo.6@hotmail.com>
2022-05-31 16:00:13 -0400

    31 Mayo

--- Unidad 2/DOM Interaccion/data.js
+++ Unidad 2/DOM Interaccion/data.js
@@ -34,7 +34,8 @@ for (let i = 0; i < Personas.length; i++) {
     card.classList.add("card")
     card.classList.add("col-md-3")
     card.classList.add("p-3")
-    card.style.height = "300px"
+    card.classList.add("m-3")
+    //card.style.height = "300px"
 
     const tituloName = document.createElement("h2")
     tituloName.innerHTML = Item.Nombre;
@@ -45,9 +46,72 @@ for (let i = 0; i < Personas.length; i++) {
     tituloApe.className = "fst-italic"
     tituloApe.classList.add("text-center")
     tituloApe.innerHTML = Item.Apellidos
-
     card.appendChild(tituloApe)
 
+    const edad = document.createElement("p")
+    edad.className = "fst-italic"
+    edad.classList.add("text-center")
+    edad.innerHTML = "Edad: " + Item.Age
+    card.appendChild(edad)
+
+
+    const city = document.createElement("p")
+    city.className = "fst-italic"
+    city.classList.add("text-center")
+    city.innerHTML = "Ciudad: " + Item.Ciudad
+    card.appendChild(city)
+
+    const ul = document.createElement("ul")
+    ul.className = "list-group"
+    ul.classList.add("text-center")
+    Item.PreferenciasMusicales.forEach(p => {
+        const li = document.createElement("li")
+        li.innerHTML = p
+        li.className = "list-group-item"
+        ul.appendChild(li)
+    })
+
+    card.appendChild(ul)
+
+    const btn = document.createElement("button")
+    btn.innerHTML = "Test"
+    btn.className = "btn"
+    btn.classList.add("btn-outline-success")
+    btn.classList.add("m-2")
+
+    
+    const btnBorrar = document.createElement("button")
+    btnBorrar.innerHTML = "Delete"
+    btnBorrar.className = "btn"
+    btnBorrar.classList.add("btn-danger")
+    btnBorrar.classList.add("m-2")
+
+
+    btn.addEventListener("click", function(){
+        alert("Click")
+    })
+    
+    btnBorrar.addEventListener("click", function(e){
+        root.removeChild(card)
+        console.log(e.target)
+    })
+
+    //btnBorrar.addEventListener("click", BorrarCard(root, card))
+
+    card.addEventListener("mouseover", function(){
+        //card.style.backgroundColor = "lightblue"
+        card.classList.add("shadow")
+    })
+    card.addEventListener("mouseleave", function(){
+        //card.style.backgroundColor = "white"
+        card.classList.remove("shadow")
+    })
+
+    card.appendChild(btn)
+    card.appendChild(btnBorrar)
     root.appendChild(card)   
 }
 
+function BorrarCard(container, remove){
+    container.removeChild(remove)
+}
